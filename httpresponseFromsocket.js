220f20e4245149e7866fe72807e6b341fa2e9e93
ramasubbareddy1224 <ramasubbareddy24@gmail.com>
2020-10-01 16:59:26 +0530

    Update httpresponseFromsocket.js

--- httpresponseFromsocket.js
+++ httpresponseFromsocket.js
@@ -1,3 +1,33 @@
+const net = require('net');
+const port = 7071;
+const host = '127.0.0.1';
+
+const server = net.createServer();
+server
+    .on('connection', (socket) => {
+
+        console.log('CONNECTED: ' + socket.remoteAddress + ':' + socket.remotePort);
+        socket.on('data', (data) => {
+            console.log('client data:', data.toString());
+            socket.write("HTTP/1.1 200 OK\nContent-Type: text/plain\nContent-Length: 11\n\nHello world");
+        });
+        socket.on('close', function (data) {  // this will trigger when client socket disconnected            
+            console.log('CLOSED: ' + socket.remoteAddress + ' ' + socket.remotePort);
+        });
+        socket.on('error', function (data) {  // this will trigger when client socket disconnected        
+            console.log('ERROR: ' + socket.remoteAddress + ' ' + socket.remotePort);
+        });        
+    })
+    .on('error', (err) => {
+        console.error(err);
+    })
+    .listen(port,host,()=>{
+        console.log('TCP Server is running on port ' + port + '.');
+    });
+
+
+
+// another example
 var http = require('http');
 
 http.createServer(onReq).listen(7070);
